4b92459cd0e9b4e60f051faa527f720ee32b4d19
amirhossein allami <amirallami.dev@gmail.com>
2025-02-02 15:06:58 +0330

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,79 +1,76 @@
 let $ = document
-const guessTextElem = $.querySelector(".text-guess")
-const guessTextElem2 = $.querySelector(".text-guess2")
-const guessTextElem3 = $.querySelector(".text-guess3")
-const buttonsWrapper = $.querySelector(".buttons-div")
-const headerText = $.querySelector(".header-text")
-const okBtn = $.querySelector("#OK")
-const yesBtn = $.querySelector("#Yes")
-const noBtn = $.querySelector("#No")
-const lowerBtn = $.querySelector("#Lower")
-const higherBtn = $.querySelector("#Higher")
-const againBtn = $.querySelector("#Again")
+const text_element = $.querySelector(".text-guess")
+const second_text_element = $.querySelector(".text-guess2")
+const buttons_container = $.querySelector(".buttons-div")
+const holder_container = $.querySelector(".titles-holder")
+const okay_button = $.querySelector("#OK")
+const yes_button = $.querySelector("#Yes")
+const no_button = $.querySelector("#No")
+const lower_button = $.querySelector("#Lower")
+const higher_button = $.querySelector("#Higher")
+const again_button = $.querySelector("#Again")
 
+let numbers_len = 1000
 let allNumbers = []
-let allNumbersLength = 1000
 let shownNumbers = []
 
-for (let i = 1; i <= allNumbersLength; i++) {
+for (let i = 1; i <= numbers_len; i++) {
     allNumbers.push(i);
 }
-headerText.innerHTML = `I will find your picked number, between ${allNumbersLength} numbers in the lowest guesses`
-
-guessTextElem.innerHTML = "choose a number between " + allNumbers[0] + " and " + allNumbers.length + " (keep this number in mind)"
-
-guessTextElem2.innerHTML = "and Click on \" OK \" Button to Continue"
+holder_container.insertAdjacentHTML('beforeend', `<p class="header-text">I will find your chosen number, between ${numbers_len} numbers in the lowest estimates</p>`)
+text_element.innerHTML = `choose a number between ${allNumbers[0]} and ${allNumbers.length} (keep this number in mind)`
+second_text_element.innerHTML = `Click on " OK " Button to Continue \n <p class="text-guess3"><span class="Bold">Attention!</span> If you answer even one of the questions <br />incorrectly, the number will not be found.</p>`
 
 let low = allNumbers[0]
 let mid = allNumbers.length / 2
 let high = allNumbers.length
 let guessCounter = 1
 
-let guessUserNumber = () => {
-    guessTextElem2.style.display = 'none'
-    guessTextElem3.style.display = 'none'
-    okBtn.style.display = 'none'
-    yesBtn.style.display = 'inline'
-    noBtn.style.display = 'inline'
-    guessTextElem.innerHTML = mid + " is your number? "
-    buttonsWrapper.addEventListener("click", stepTwo)
+let step_one = () => {
+    second_text_element.style.display = 'none'
+    okay_button.style.display = 'none'
+    yes_button.style.display = 'inline'
+    no_button.style.display = 'inline'
+    text_element.innerHTML = `is ${mid} your Number?`
+    buttons_container.addEventListener("click", step_two)
 }
-let stepTwo = event => {
+let step_two = event => {
+    console.log(event)
     if (event.target.innerHTML === "Yes") {
-        guessTextElem.style.display = 'none'
-        guessTextElem2.style.display = 'flex'
-        guessTextElem2.innerHTML = "I found your number with only one estimate 😎 your number is " + mid
-        yesBtn.style.display = 'none'
-        noBtn.style.display = 'none'
-        againBtn.style.display = 'inline'
-        againBtn.addEventListener('click', AgainFunction)
+        text_element.style.display = 'none'
+        second_text_element.style.display = 'flex'
+        second_text_element.innerHTML = "I found your number with only one estimate 😎 your number is " + mid
+        yes_button.style.display = 'none'
+        no_button.style.display = 'none'
+        again_button.style.display = 'inline'
+        again_button.addEventListener('click', reload_fn)
     } else if (event.target.innerHTML === "No") {
-        yesBtn.style.display = 'none'
-        noBtn.style.display = 'none'
-        lowerBtn.style.display = 'inline'
-        higherBtn.style.display = 'inline'
+        yes_button.style.display = 'none'
+        no_button.style.display = 'none'
+        lower_button.style.display = 'inline'
+        higher_button.style.display = 'inline'
         arrayChecker(shownNumbers, mid)
     }
 }
 let arrayChecker = (array, mid) => {
     array.push(mid)
-    yesBtn.innerHTML = "Yes, it's my number 👍"
+    yes_button.innerHTML = "Yes, it's my number 👍"
     if (mid == allNumbers.length / 2) {
-        guessTextElem.innerHTML = `is your number lower than <span class="Bold">${mid}</span> or higher?`
+        text_element.innerHTML = `is your number lower than <span class="Bold">${mid}</span> or higher?`
     } else {
-        guessTextElem.innerHTML = `is <span class="Bold">${mid}</span> your number? If no, is your number lower than <span class="Bold">${mid}</span> or higher?`
+        text_element.innerHTML = `is <span class="Bold">${mid}</span> your number? If no, is your number lower than <span class="Bold">${mid}</span> or higher?`
     }
-    buttonsWrapper.addEventListener("click", stepThree)
+    buttons_container.addEventListener("click", step_three)
 }
-let stepThree = event => {
-    yesBtn.style.display = 'inline'
+let step_three = event => {
+    yes_button.style.display = 'inline'
     guessCounter++
     if (mid === (allNumbers[0] + 1)) {
         mid = allNumbers[0]
-        showResult()
+        show_result()
     } else if (mid === allNumbers.length - 1) {
         mid = allNumbers.length
-        showResult()
+        show_result()
     } else {
         if (event.target.id === "Lower") {
             mid -= 2
@@ -82,7 +79,7 @@ let stepThree = event => {
             })
             if (numberFinder.length > 0) {
                 mid += 1
-                showResult()
+                show_result()
             } else {
                 mid += 2
                 high = mid
@@ -95,29 +92,29 @@ let stepThree = event => {
             })
             if (numberFinder.length > 0) {
                 mid -= 1
-                showResult()
+                show_result()
             } else {
                 mid -= 2
                 low = mid
                 mid = mid + Math.ceil(((high - low) / 2))
             }
         } else if (event.target.id === "Yes") {
-            showResult()
+            show_result()
         }
         arrayChecker(shownNumbers, mid)
     }
 }
-let showResult = () => {
-    guessTextElem.style.display = 'none'
-    guessTextElem2.style.display = 'flex'
-    guessTextElem2.innerHTML = "i beat you with only " + guessCounter + " questions 😎 your number is " + mid
-    yesBtn.style.display = 'none'
-    lowerBtn.style.display = 'none'
-    higherBtn.style.display = 'none'
-    againBtn.style.display = 'inline'
-    againBtn.addEventListener('click', AgainFunction)
+let show_result = () => {
+    text_element.style.display = 'none'
+    second_text_element.style.display = 'flex'
+    second_text_element.innerHTML = "i beat you with only " + guessCounter + " questions 😎 your number is " + mid
+    yes_button.style.display = 'none'
+    lower_button.style.display = 'none'
+    higher_button.style.display = 'none'
+    again_button.style.display = 'inline'
+    again_button.addEventListener('click', reload_fn)
 }
-let AgainFunction = () => {
+let reload_fn = () => {
     location.reload()
 }
-okBtn.addEventListener('click', guessUserNumber)
+okay_button.addEventListener('click', step_one)
